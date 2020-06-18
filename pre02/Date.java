ac7e9bd22410f58f660c9fefba4dc202e818692f
Margie MonteLeon <mmonteleon@mac.com>
2020-06-18 10:00:57 -0400

    Exercise 2 Section 2.12

---
+++ pre02/Date.java
@@ -0,0 +1,19 @@
+public class Date
+{
+  public static void main(String[] args)
+  {
+    String day = "Thursday";
+    String month = "June";
+    int date = 18;
+    int year = 2020;
+
+    System.out.println("American Format:");
+    System.out.println(day + ", " + month + " " + date + ", " + year);
+
+    System.out.println("European Format:");
+    System.out.println(day + " " + date + " " + month + " " + year);
+
+
+
+  }
+}
