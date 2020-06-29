d602228bf3f7c376c7020be57fc2b402af4725b3
Margie MonteLeon <mmonteleon@mac.com>
2020-06-29 14:50:39 -0400

    RecursiveSum2 added

--- 00 Hunter/RecursiveSum.java
+++ 00 Hunter/RecursiveSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class RecursiveSum
 {
   public static void main(String[] args) {
@@ -6,6 +7,7 @@ public class RecursiveSum
       arr[i] = i;
     System.out.println("Sum of digits between 0 & 5 inclusive: " +recursiveSum(arr, 0, 5));
     System.out.println("Sum of digits between 0 & 99 inclusive: " +recursiveSum(arr, 0, 99));
+    System.out.println("Sum of Elements in array: " +recursiveSum2(arr));
 
   }
 
@@ -16,4 +18,12 @@ public class RecursiveSum
       return ia[start] + recursiveSum(ia, start+1, end);
 
     }
+
+   public static int recursiveSum2(int[] ia)
+   {
+     if(ia.length == 0)
+      return 0;
+      else
+        return ia[0] + recursiveSum2(Arrays.copyOfRange(ia, 1, ia.length));
+   }
 }
