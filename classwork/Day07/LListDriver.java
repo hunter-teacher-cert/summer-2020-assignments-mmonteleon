3763100af653609588b2f0ae6d1d203b8b49d747
Margie MonteLeon <mmonteleon@mac.com>
2020-07-09 14:07:49 -0400

    added try, catch

---
+++ classwork/Day07/LListDriver.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class LListDriver {
+  public static void main(String[] args) {
+
+    LList g = new LList();
+
+    System.out.println("Test Empty list print & isEmpty:");
+    System.out.println(g);
+    System.out.println(g.isEmpty());
+    System.out.println("Length: " + g.length());
+
+
+    System.out.println("\nTest addFront 3x & isEmpty():");
+    g.addFront("good");
+    g.addFront("news");
+    g.addFront("everyone!");
+    System.out.println(g);
+    System.out.println("Length: " + g.length());
+    System.out.println(g.isEmpty());
+
+   System.out.println("\nTest get 0, 1, 5:");
+   System.out.println(g.get(0));
+   System.out.println(g.get(1));
+   try
+   {
+     System.out.println(g.get(5));
+   }
+   catch(IndexOutOfBoundsException e)
+   {
+     System.out.println("Nah boo.  You can't get what doesn't exist!");
+   }
+
+
+
+   System.out.println("\nTest set 0, 1, 5:");
+   g.set(0, "me!");
+   g.set(1, "job");
+   try
+   {
+     g.set(5, "wow");
+   }
+   catch(IndexOutOfBoundsException e)
+   {
+     System.out.println("Nah boo.  You can't set what doesn't exist!");
+   }
+   System.out.println(g);
+
+   System.out.println("\nTest insert 0, 2, 5, 10:");
+   g.insert(0, "woo!");
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+   g.insert(2, "go");
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+   g.insert(5, "cool");
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+   try
+   {
+     g.insert(10, "too far");
+   }
+   catch(IndexOutOfBoundsException e)
+   {
+     System.out.println("Nah boo.  You can't insert there!");
+   }
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+
+   System.out.println("\nTest search \"woo!\", \"cool\", \"too far\"");
+   System.out.println(g.search("woo!"));
+   System.out.println(g.search("cool"));
+   System.out.println(g.search("too far"));
+
+   System.out.println("\nTest remove 0, 2:");
+   g.remove(0);
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+   g.remove(2);
+   System.out.println(g);
+   System.out.println("Length: " + g.length());
+
+  }//main
+}//LListDriver
