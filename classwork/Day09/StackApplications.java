ef45c6fb1e2d4a1a94907c6da34c91c34a048343
Margie MonteLeon <mmonteleon@mac.com>
2020-07-13 14:22:42 -0400

    StackApp w paren

--- classwork/Day09/StackApplications.java
+++ classwork/Day09/StackApplications.java
@@ -78,4 +78,26 @@ public class StackApplications
     return s.equals(reverseWords(s));
   }
 
+  //Didn't use teh stack on this one....
+  public static boolean parenCheck(String s)
+  {
+    int open = 0, close = 0;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+      char ch = s.charAt(i);
+      if(ch == '(')
+        open++;
+      else if(ch == ')')
+        close++;
+
+      if(close > open)
+        return false;
+    }
+
+    return open == close;
+  }
+
+
 }//End Class
